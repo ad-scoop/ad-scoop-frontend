7605d5a66f7abf20f0936b136d16f49b756ccef2
Tim Gundmann <tim@gundmann.dk>
2017-01-07 16:29:52 +0100

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { TopMenu } from './components/topmenu/top.menu';
+//import { TopMenu } from './components/topmenu/top.menu';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TopMenu
+    AppComponent
+    //TopMenu
   ],
   imports: [
     BrowserModule,
