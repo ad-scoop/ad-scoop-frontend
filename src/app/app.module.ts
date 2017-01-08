63352acdc9c1d7e4fe53c34838abbcf880ff446c
Tim Gundmann <tim@gundmann.dk>
2017-01-08 22:35:33 +0100

    front page progressing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TopMenu } from './components/topmenu/top.menu';
+import { appRoutes } from './app.routes';
+import { Home } from './components/home/home';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    Home,
     TopMenu
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(appRoutes),
     HttpModule
   ],
   providers: [],
