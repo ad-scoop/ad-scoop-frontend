9ed9dcd235f89b6629df883c03a104e110dbe1a7
tim <tim@DESKTOP-LBC97U1.lan>
2017-01-21 21:30:03 +0100

    fix for upper case

--- src/app/components/underconstruction/under.construction.ts
+++ src/app/components/underconstruction/under.construction.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'underConstruction',
-  templateUrl: './underConstruction.html'
+  templateUrl: './under.onstruction.html'
 })
 export class UnderConstruction {
 }
