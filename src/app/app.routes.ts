9865298fb30ab0a0861834af62f2a8d4b3898ae8
tim <tim@DESKTOP-LBC97U1.lan>
2017-01-21 21:25:50 +0100

    fix for upper case

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { Login } from './components/login/login';
-import { UnderConstruction } from './components/underConstruction/underConstruction';
+import { UnderConstruction } from './components/underconstruction/under.construction';
 
 export const appRoutes: Routes = [
   { path: '', component: Home },
