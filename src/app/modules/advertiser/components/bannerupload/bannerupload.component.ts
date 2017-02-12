5fa060a02bdee8cedeb158298be7baf963d8cb02
Tim Gundmann <tim@gundmann.dk>
2017-02-12 15:56:29 +0100

    fix

--- src/app/modules/advertiser/components/bannerupload/bannerupload.component.ts
+++ src/app/modules/advertiser/components/bannerupload/bannerupload.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-bannerupload',
-  templateUrl: './banneruplad.component.html',
-  styleUrls: ['./banneruplad.component.css']
+  templateUrl: './bannerupload.component.html',
+  styleUrls: ['./bannerupload.component.css']
 })
 export class BannerUploadComponent implements OnInit {
 
