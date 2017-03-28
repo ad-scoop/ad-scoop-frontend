d73c5cf8855b798744f6c3c7efcef59d553b2506
Tim Gundmann <tim@gundmann.dk>
2017-03-28 20:39:58 +0200

    environment changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  userUrl: 'http://ad-scoop.com:8181/user',
-  publisherUrl: 'http://ad-scoop.com:8182/campagin',
+  userUrl: 'http://ad-scoop.com/user',
+  publisherUrl: 'http://ad-scoop.com/publisher/campagin',
   maxPictureSize: 512000,
 };
