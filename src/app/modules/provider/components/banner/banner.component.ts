9137a218924ba7acc263619b979ef1d32e9347d9
Tim Gundmann <tim@gundmann.dk>
2017-04-12 19:14:14 +0200

    some updates

--- src/app/modules/provider/components/banner/banner.component.ts
+++ src/app/modules/provider/components/banner/banner.component.ts
@@ -4,7 +4,10 @@ import { PlaceType } from '../../../../model/placetype';
 import { WebSite } from '../../../../model/site';
 import { SiteService } from '../../../../services/site.service';
 import { EditInterface } from '../editdialog/editinterface';
-import { Component, OnInit, Input, AfterViewInit, ViewChild, ViewContainerRef, ComponentFactoryResolver } from '@angular/core';
+import {
+  Component, OnInit, Input, AfterViewInit, ViewChild,
+  ViewContainerRef, ComponentFactoryResolver,
+} from '@angular/core';
 
 @Component({
   selector: 'app-banner',
@@ -16,7 +19,7 @@ export class BannerComponent implements EditInterface, AfterViewInit {
   @Input() site: WebSite;
   @ViewChild('site') siteCanvas;
 
-  htmlText: string;
+  private canvaswidth = 500;
 
   model: BannerSpace = new BannerSpace();
   bannerLocations = [];
@@ -37,8 +40,10 @@ export class BannerComponent implements EditInterface, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    //    let canvas = this.siteCanvas.nativeElement;
-    //    this.context = canvas.getContext('2d');
+    const canvas = this.siteCanvas.nativeElement;
+    canvas.width = this.canvaswidth;
+    canvas.height = 400;
+    this.context = canvas.getContext('2d');
 
     this.draw();
   }
@@ -73,58 +78,59 @@ export class BannerComponent implements EditInterface, AfterViewInit {
   }
 
   draw(): void {
-    this.site.bannerSpaces.forEach(b => {
-      this.htmlText = '<div>test</div>';
+    requestAnimationFrame(() => {
+      this.draw();
     });
 
-    //    requestAnimationFrame(() => {
-    //      this.draw();
-    //    });
-    //
-    //    this.drawCanvas();
-    //    this.site.bannerSpaces.forEach(b => {
-    //      this.drawBannerSpace(b);
-    //    });
+    this.drawCanvas();
+    this.site.bannerSpaces.forEach(b => {
+      this.drawBannerSpace(b);
+    });
   }
 
   private drawCanvas(): void {
-    this.context.clearRect(0, 0, 300, 150);
+    this.context.clearRect(0, 0, this.canvaswidth, 400);
     this.context.beginPath();
     this.context.lineWidth = 1;
     this.context.strokeStyle = 'black';
-    this.context.rect(0, 0, 300, 150);
+    this.context.rect(0, 0, this.canvaswidth, 400);
     this.context.stroke();
 
     this.context.fillStyle = '#f0f0f0';
     this.context.font = '30px Roboto, sans-serif';
-    this.context.fillText('Webseite', 80, 85);
+    this.context.fillText('Webseite', 180, 185);
   }
 
   private drawBannerSpace(bannerSpace: BannerSpace) {
     this.context.beginPath();
     this.context.lineWidth = 1;
     this.context.strokeStyle = 'red';
-    let place = this.calculatePlace(bannerSpace);
-    this.context.rect(place.x, place.y, bannerSpace.width / 3, bannerSpace.height / 3);
+    const place = this.calculatePlace(bannerSpace);
+    this.context.rect(place.x, place.y, bannerSpace.width / 2, bannerSpace.height / 2);
     this.context.stroke();
 
-    this.context.fillStyle = 'red';
-    this.context.font = '10px Roboto, sans-serif';
-    this.context.fillText(bannerSpace.width + ' x ' + bannerSpace.height, place.x + 2, place.y + 10);
+    this.context.strokeStyle = 'red';
+    this.context.font = '9px Roboto, sans-serif';
+    this.context.strokeText(bannerSpace.width + ' x ' + bannerSpace.height, place.x + 2, place.y + 10);
+
   }
 
   private calculatePlace(bannerSpace: BannerSpace): Position {
     if (PlaceType.Top === bannerSpace.place) {
-      return new Position(0, 0);
+      const left = (this.canvaswidth - (bannerSpace.width / 2)) / 2;
+      return new Position(left, 1);
     }
     if (PlaceType.Bottom === bannerSpace.place) {
-      return new Position(0, 150 - (bannerSpace.height / 3));
+      return new Position(1, 150 - (bannerSpace.height / 3));
     }
     if (PlaceType.Define === bannerSpace.place) {
       return new Position(bannerSpace.left, bannerSpace.top);
     }
     if (PlaceType.Right === bannerSpace.place) {
-      return new Position(300 - (bannerSpace.width / 3), 0);
+      return new Position(300 - (bannerSpace.width / 3), 30);
+    }
+    if (PlaceType.Left === bannerSpace.place) {
+      return new Position(0, 30);
     }
     return new Position(0, 0);
   }
