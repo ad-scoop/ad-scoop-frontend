311b8343df335f5bbaf1a196e50035617fc18986
Tim Gundmann <tim@gundmann.dk>
2017-04-29 11:40:53 +0200

    change to https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   userUrl: 'https://ad-scoop.com/user',
   publisherUrl: 'https://ad-scoop.com/publisher/campagins',
-  websiteUrl: 'http://ad-scoop.com/website',
+  websiteUrl: 'https://ad-scoop.com/website',
   maxPictureSize: 512000,
 };
